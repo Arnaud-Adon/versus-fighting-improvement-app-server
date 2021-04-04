06345a0f6d28e46a876f45cb46de4bb4efce0b4a
Arnaud-Adon <assy.adon@yahoo.fr>
2021-04-04 17:14:07 +0200

    (fix) écoute du port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
-// const http = require("http");
+const http = require("http");
 const config = require("./config");
 const databaseConnexion = require("./services/connexion");
 const routes = require("./routes");
@@ -11,16 +11,13 @@ const routeApi = express.Router();
 app.use("/api", routeApi);
 
 routeApi.use(bodyParser.json({ type: "*/*" }));
-// routeApi.use(bodyParser.json());
 routeApi.use(cors());
 routeApi.use(morgan("common"));
 
 databaseConnexion();
 
 routes(routeApi);
-// const server = http.createServer(app);
-routeApi.listen(config.port, function () {
+const server = http.createServer(app);
+server.listen(config.port, function () {
   console.log(`Server listen on port ${config.port}`);
 });
-
-// module.exports = app;
